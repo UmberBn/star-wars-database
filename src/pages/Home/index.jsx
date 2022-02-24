11a21832990ed4d341431650cf90ad8255dd5375
UmberBn <humberto-bandeira@hotmail.com>
2022-02-24 01:05:57 -0400

    feature: add styles for contain table overflow

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,107 @@
+import { Col, Empty, Form, Input, Row, Table } from "antd";
+import React, { useContext } from "react";
+import PlanetsContext from "../../context/PlanetsContext";
+import { Container, FiltersContainer, FiltersPanel } from "./styles";
+
+function Home() {
+  const { filteredPlanets } = useContext(PlanetsContext);
+  const columns = [
+    {
+      title: "Nome",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Período de Rotação",
+      dataIndex: "rotation_period",
+      key: "rotation_period",
+    },
+    {
+      title: "Período de Orbita",
+      dataIndex: "orbital_period",
+      key: "orbital_period",
+    },
+    {
+      title: "Diâmetro",
+      dataIndex: "diameter",
+      key: "diameter",
+    },
+    {
+      title: "Clima",
+      dataIndex: "climate",
+      key: "climate",
+    },
+    {
+      title: "Gravidade",
+      dataIndex: "gravity",
+      key: "gravity",
+    },
+    {
+      title: "Terreno",
+      dataIndex: "terrain",
+      key: "terrain",
+    },
+    {
+      title: "Água na superfície",
+      dataIndex: "surface_water",
+      key: "surface_water",
+    },
+    {
+      title: "População",
+      dataIndex: "population",
+      key: "population",
+    },
+    {
+      title: "Filmes",
+      dataIndex: "films",
+      key: "films",
+      render: (text, _movies, index) => {
+        return text.map((movie) => (
+          <span key={`${movie}${index}`}>{movie}</span>
+        ));
+      },
+    },
+    {
+      title: "Criado",
+      dataIndex: "created",
+      key: "created",
+    },
+    {
+      title: "Editado",
+      dataIndex: "edited",
+      key: "edited",
+    },
+    {
+      title: "Link",
+      dataIndex: "url",
+      key: "url",
+    },
+  ];
+  return (
+    <Container>
+      <FiltersContainer bordered={false} defaultActiveKey={["1"]}>
+        <FiltersPanel header="Filtros" key="1">
+          <Form layout="vertical" wrapperCol={6}>
+            <Row>
+              <Col span={8}>
+                <Form.Item label="Nome" name="name" wrapperCol={12}>
+                  <Input />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </FiltersPanel>
+      </FiltersContainer>
+      Home{" "}
+      <Table
+        dataSource={filteredPlanets}
+        columns={columns}
+        locale={{
+          emptyText: <Empty description="Nenhum planeta encontrado 🤖" />,
+        }}
+      />
+    </Container>
+  );
+}
+
+export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,10 @@
 import { Collapse } from "antd";
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  overflow: scroll;
+  overflow-y: hidden
+`;
 
 export const FiltersContainer = styled(Collapse)``;
 
