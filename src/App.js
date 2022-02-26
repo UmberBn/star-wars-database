44c4c2fef8d5c41ba209705acb8b9f0794d3e994
UmberBn <humberto-bandeira@hotmail.com>
2022-02-26 17:52:41 -0400

    refactor: create app.css for import antd css, removed react logo file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout } from "./components";
 import { Home } from "./pages";
 import GlobalContext from "./context";
+import "./App.css"
 
 function App() {
   return (
